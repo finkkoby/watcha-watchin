eab3f2f51c742f338834df5ffee015c779e27412
Koby Fink <finkkoby@icloud.com>
2024-05-08 14:49:28 -0400

    add form data for join page

--- client/src/pages/Join.js
+++ client/src/pages/Join.js
@@ -1,9 +1,41 @@
-
+import * as yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 function Join() {
+
+    const formSchema = yup.object().shape({
+        name: yup.string().required(),
+        roomCode: yup.string().required()
+    })
+
     return (
-        <div className='page-header'>
-            <h3>Join Page</h3>
+        <div className='form-container'>
+            <Formik
+                initialValues={{
+                    name: '',
+                    roomCode: ''
+                }}
+                validationSchema={formSchema}
+                onSubmit={values => console.log(values)}
+            >
+                {props => (
+                    <Form>
+                        <label htmlFor="name">
+                            name
+                            <Field name="name" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="name" component="p" />
+
+                        <label htmlFor="roomCode">
+                            room code
+                            <Field name="roomCode" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="roomCode" component="p" />
+
+                        <button type='submit'>- join room -</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
