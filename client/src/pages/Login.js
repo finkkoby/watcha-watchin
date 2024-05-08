86285deb293f4639c9f282b80d55e614664180d5
Koby Fink <finkkoby@icloud.com>
2024-05-08 14:47:53 -0400

    add form data for Signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import * as yup from 'yup'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,81 @@
-
+import * as yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 function Signup() {
+    
+    const formSchema = yup.object().shape({
+        firstName: yup.string().required("please enter your first name"),
+        lastName: yup.string().required("please enter your last name"),
+        username: yup.string().required("please enter a username"),
+        email: yup.string().required("please enter an email").email("please enter a valid email"),
+        password: yup.string().required("please enter a password"),
+        confirmPassword: yup.string().required("please confirm your password").oneOf([yup.ref('password'), null], "passwords do not match"),
+        age: yup.number().required("please enter your age").min(18, "you must be at least 18 years old to register")
+    })
+
     return (
-        <div className='page-header'>
-            <h3>Signup Page</h3>
+        <div className="form-container">
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    username: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: '',
+                    age: '',
+                }}
+                validationSchema={formSchema}
+                onSubmit={values => console.log(values)}
+            >
+                {props => (
+                    <Form>
+                        <label htmlFor="firstName">
+                            enter your first name
+                            <Field name="firstName" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="firstName" component="p" />
+                        
+                        <label htmlFor="lastName">
+                            enter your last name
+                            <Field name="lastName" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="lastName" component="p" />
+
+                        <label htmlFor="username">
+                            create a username
+                            <Field name="username" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="username" component="p" />
+
+                        <label htmlFor='email'>
+                            enter your email
+                            <Field name="email" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="email" component="p" />
+                        
+                        <label htmlFor='password'>
+                            create password
+                            <Field name="password" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="password" component="p" />
+
+                        <label htmlFor='confirmPassword'>
+                            confirm password
+                            <Field name="confirmPassword" placeholder="required" />
+                        </label>
+                        <ErrorMessage name="confirmPassword" component="p" />
+
+                        <label htmlFor='age'>
+                            enter your age
+                            <Field name="age" placeholder="optional" />
+                        </label>
+                        <ErrorMessage name="age" component="p" />
+
+                        <button type='submit'>- signup -</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
