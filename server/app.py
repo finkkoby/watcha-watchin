f3bc2409a9de3661493f798e6e4c615e3757bdea
Koby Fink <finkkoby@icloud.com>
2024-05-09 12:42:19 -0400

    configure password methods for User model, configure API resource for Login POST request

--- server/app.py
+++ server/app.py
@@ -15,7 +15,22 @@ class Index(Resource):
     def get(self):
         return {'message': 'Welcome to my REST API'}
     
+class Login(Resource):
+    def post(self):
+        json = request.get_json()
+        try:
+            user = User.query.filter_by(username=json['username']).first()
+            if user:
+                if user.autheticate(json['password']):
+                    session['user_id'] = user.id
+                    return user.to_dict(), 200
+                else:
+                    return {'message': 'Invalid credentials'}, 401
+        except:
+            return {'message': 'User not found'}, 404
+    
 api.add_resource(Index, '/')
+api.add_resource(Login, '/api/login')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
