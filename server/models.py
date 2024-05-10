8da75a75a5749ac3322cacc3c6c98d049b881e10
Koby Fink <finkkoby@icloud.com>
2024-05-10 13:53:02 -0400

    create rooms table, establish room and user relationship

--- server/models.py
+++ server/models.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import string
+import random
+
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import relationship, validates
@@ -18,12 +21,13 @@ class User(db.Model, SerializerMixin):
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     age = db.Column(db.Integer)
+    room_id = db.Column(db.String, db.ForeignKey('rooms.id'), default=None,)
 
     # Relationships
-
+    room = db.relationship("Room", back_populates="users")
 
     # Serialize Rules
-
+    serialize_rules = ("-room.users",)
 
     # Validations
     @hybrid_property
@@ -39,3 +43,24 @@ class User(db.Model, SerializerMixin):
     # Other Methods
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+class Room(db.Model, SerializerMixin):
+    __tablename__ = 'rooms'
+
+    # Database Schema
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    code = db.Column(db.String(5), unique=True)
+
+    # Relationships
+    users = db.relationship("User", back_populates="room")
+
+    # Serialize Rules
+    serialize_rules = ("-users.room",)
+
+    # Validations
+
+
+    # Other Methods
+
+
