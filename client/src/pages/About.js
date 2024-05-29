5a0fd5ec42ac7c5b6a39b6c8acb612e5ea545209
Koby Fink <finkkoby@icloud.com>
2024-05-29 16:08:05 -0400

    reformat about.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,7 +3,12 @@
 function About() {
     return (
         <div className='about-body'>
-            <p>The wonderful viewing synchronicity of the beloved NETFLIX PARTY with the practicality and relatability of YouTube. No more grainy videos via Zoom or counting down and pressing play on two different devices at the same. We'll take care of all of that for you. Get <em>watchin'</em> now!</p>
+            <p>The wonderful viewing synchronicity of the beloved 
+                NETFLIX PARTY with the practicality and relatability 
+                of YouTube. No more grainy videos via Zoom or counting 
+                down and pressing play on two different devices at the 
+                same. We'll take care of all of that for you. Get 
+                <em>watchin'</em> now!</p>
         </div>
     )
 }
